b24242bd73c52a89afc89b2ca7d1be347fcbc816
M. Brent Harp <brharp@uoguelph.ca>
2015-03-13 17:25:55 -0400

    Rename standard layouts.

--- profiles/ug/themes/ug/ug_theme/layouts/aside/aside.tpl.php
+++ profiles/ug/themes/ug/ug_theme/layouts/aside/aside.tpl.php
@@ -4,27 +4,25 @@
  * Template for a 2 column panel layout.
  *
  * This template provides a two column panel display layout, with
- * each column equal in width.
+ * a (8:4) ratio between column widths.
  *
  * Variables:
  * - $id: An optional CSS id to use for the layout.
  * - $content: An array of content, each item in the array is keyed to one
  *   panel of the layout. This layout supports the following sections:
+ *   - $content['top']: Content in the top row.
  *   - $content['left']: Content in the left column.
  *   - $content['right']: Content in the right column.
+ *   - $content['bottom']: Content in the bottom row.
  */
 ?>
-<?php if (!empty($content['top'])): ?>
 <div class="row">
-  <div class="col-md-12"><?php print $content['top']; ?></div>  
+  <div class="col-sm-12"><?php print $content['top']; ?></div>  
 </div>
-<?php endif; ?>
 <div class="row">
-  <div class="col-md-6"><?php print $content['left']; ?></div>
-  <div class="col-md-6"><?php print $content['right']; ?></div>
+  <div class="col-sm-8"><?php print $content['left']; ?></div>
+  <div class="col-sm-4"><?php print $content['right']; ?></div>
 </div>
-<?php if (!empty($content['bottom'])): ?>
 <div class="row">
-  <div class="col-md-12"><?php print $content['bottom']; ?></div>
+  <div class="col-sm-12"><?php print $content['bottom']; ?></div>
 </div>
-<?php endif; ?>
--- profiles/ug/themes/ug/ug_theme/layouts/front/front.tpl.php
+++ profiles/ug/themes/ug/ug_theme/layouts/front/front.tpl.php
@@ -1,30 +1,29 @@
 <?php
 /**
  * @file
- * Template for a 2 column panel layout.
+ * Template for a 3 column panel layout.
  *
- * This template provides a two column panel display layout, with
+ * This template provides a three column panel display layout, with
  * each column equal in width.
  *
  * Variables:
  * - $id: An optional CSS id to use for the layout.
  * - $content: An array of content, each item in the array is keyed to one
  *   panel of the layout. This layout supports the following sections:
+ *   - $content['top']: Content in the top row.
  *   - $content['left']: Content in the left column.
- *   - $content['right']: Content in the right column.
+ *   - $content['middle']: Content in the centre column.
+ *   - $content['bottom']: Content in the bottom row.
  */
 ?>
-<?php if (!empty($content['top'])): ?>
 <div class="row">
   <div class="col-md-12"><?php print $content['top']; ?></div>  
 </div>
-<?php endif; ?>
 <div class="row">
   <div class="col-md-4"><?php print $content['left']; ?></div>
-  <div class="col-md-8"><?php print $content['right']; ?></div>
+  <div class="col-md-4"><?php print $content['middle']; ?></div>
+  <div class="col-md-4"><?php print $content['right']; ?></div>
 </div>
-<?php if (!empty($content['bottom'])): ?>
 <div class="row">
   <div class="col-md-12"><?php print $content['bottom']; ?></div>
 </div>
-<?php endif; ?>
