9d569780b29554527649efae350196aaf9e31dda
M. Brent Harp <brharp@uoguelph.ca>
2015-05-01 11:15:11 -0400

    Template file for user profile.

---
+++ profiles/ug/themes/ug/ug_theme/templates/views-view-fields--p2.tpl.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * @file
+ * Template to display one profile listing.
+ *
+ * Available variables:
+ * - $firstname: the person's first name.
+ * - $lastname: the person's last name.
+ * - $fullname: first and last name together.
+ * - $image: the profile picture.
+ * - $title: the person's title.
+ * - $unit: the person's unit.
+ * - $subunit: the person's sub-unit.
+ * - $phone: the person's telephone number.
+ * - $email: the person's email address. 
+ * - $building: office building.
+ * - $room: room number.
+ * - $lab: lab number.
+ * - $website: website link.
+ * - $about: person's bio.
+ * - $education: person's education.
+ * - $research: person's research.
+ * - $publications: a list of publications
+ * - $attachments: files attached to faculty profile.
+ *
+ * Other variables:
+ * - $view: The view in use.
+ * - $fields: an array of $field objects. Each one contains:
+ *   - $field->content: The output of the field.
+ *   - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
+ *   - $field->class: The safe class id to use.
+ *   - $field->handler: The Views field handler object controlling this field. Do not use
+ *     var_export to dump this object, as it can't handle the recursion.
+ *   - $field->inline: Whether or not the field should be inline.
+ *   - $field->inline_html: either div or span based on the above flag.
+ *   - $field->wrapper_prefix: A complete wrapper containing the inline_html to use.
+ *   - $field->wrapper_suffix: The closing tag for the wrapper.
+ *   - $field->separator: an optional separator that may appear before a field.
+ *   - $field->label: The wrap label text to use.
+ *   - $field->label_html: The full HTML of the label to use including
+ *     configured element type.
+ * - $row: The raw result object from the query, with all data it fetched.
+ *
+ */
+?>
+
+<div class="row">
+  <div class="col-md-4">
+    <img class="img-responsive" src="<?php print $image; ?>"/>
+  </div>
+  <div class="col-md-8">
+    <p class='h3'>
+      <?php print $title; if ($unit): print ', '.$unit; endif; ?>
+    </p>
+    <?php if ($subunit): ?>
+      <p class='h4'>
+        <?php print $subunit; ?>
+      </p>
+    <?php endif; ?>
+    <?php if ($email): ?>
+      <?php print t("<span class='text-muted'>@label: </span>", array('@label' => 'Email')); ?>
+      <?php print $email; ?><br/>
+    <?php endif; ?>
+    <?php if ($phone): ?>
+      <?php print t("<span class='text-muted'>@label: </span>", array('@label' => 'Phone')); ?>
+      <?php print $phone; ?><br/>
+    <?php endif; ?>
+    <?php if ($fax): ?>
+      <?php print t("<span class='text-muted'>@label: </span>", array('@label' => 'Fax')); ?>
+      <?php print $fax; ?><br/>
+    <?php endif; ?>
+    <?php if ($building && $room): ?>
+      <?php print t("<span class='text-muted'>@label: </span>", array('@label' => 'Office')); ?>
+      <?php print $building.', '.$room; ?><br/>
+    <?php endif; ?>
+    <?php if ($lab): ?>
+      <?php print t("<span class='text-muted'>@label: </span>", array('@label' => 'Lab')); ?>
+      <?php print $lab; ?><br/>
+    <?php endif; ?>
+    <?php if ($website): ?>
+      <?php print t("<span class='text-muted'>@label: </span>", array('@label' => 'Website')); ?>
+      <?php print $website; ?><br/>
+    <?php endif; ?>
+  </div>
+</div>
+<?php if ($about): ?>
+  <h2><?php print t('About'); ?></h2>
+  <?php print $about; ?>
+<?php endif; ?>
+<?php if ($education): ?>
+  <h2><?php print t('Education'); ?></h2>
+  <?php print $education; ?>
+<?php endif; ?>
+<?php if ($publications): ?>
+  <h2><?php print t('Publications'); ?></h2>
+  <?php print $publications; ?>
+<?php endif; ?>
+<?php if ($research): ?>
+  <h2><?php print t('Research'); ?></h2>
+  <?php print $research; ?>
+<?php endif; ?>
+<?php if ($attachments): ?>
+  <h2><?php print t('Relevant Files'); ?></h2>
+  <?php print $attachments; ?>
+<?php endif; ?>
+
