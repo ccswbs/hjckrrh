0a7fdf462d4f15031ebb8ea3406fd1ad896597e4
M. Brent Harp <brharp@uoguelph.ca>
2015-05-11 12:52:53 -0400

    Add first social media view.

---
+++ profiles/ug/themes/ug/ug_theme/templates/views-view-fields--s1.tpl.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @file
+ * S1 - Share this page
+ *
+ * Displays "share this page" links for a node.
+ *
+ * Available variables:
+ * - $link: URL of the current page.
+ * - $title: page title.
+ *
+ * Other variables:
+ * - $view: The view in use.
+ * - $fields: an array of $field objects. Each one contains:
+ *   - $field->content: The output of the field.
+ *   - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
+ *   - $field->class: The safe class id to use.
+ *   - $field->handler: The Views field handler object controlling this field. Do not use
+ *     var_export to dump this object, as it can't handle the recursion.
+ *   - $field->inline: Whether or not the field should be inline.
+ *   - $field->inline_html: either div or span based on the above flag.
+ *   - $field->wrapper_prefix: A complete wrapper containing the inline_html to use.
+ *   - $field->wrapper_suffix: The closing tag for the wrapper.
+ *   - $field->separator: an optional separator that may appear before a field.
+ *   - $field->label: The wrap label text to use.
+ *   - $field->label_html: The full HTML of the label to use including
+ *     configured element type.
+ * - $row: The raw result object from the query, with all data it fetched.
+ *
+ */
+?>
+
+<!-- Facebook -->
+<a class="h1" data-toggle="tooltip" data-placement="bottom"
+  href="https://www.facebook.com/sharer/sharer.php?u=<?php print $link; ?>"
+  title="<?php print t('Share on Facebook'); ?>"
+  ><span class="fa fa-facebook-square"></span></a>
+
+<!-- Twitter -->
+<a class="h1" data-toggle="tooltip" data-placement="bottom"
+  href="http://twitter.com/home?status=<?php print $link; ?>"
+  title="<?php print t('Share on Twitter'); ?>"
+  ><span class="fa fa-twitter-square"></span></a>
+
+<!-- LinkedIn -->
+<a class="h1" data-toggle="tooltip" data-placement="bottom"
+  href="http://www.linkedin.com/shareArticle?mini=true&url=&title=&summary=<?php print $link; ?>"
+  title="<?php print t('Share on LinkedIn'); ?>"
+  ><span class="fa fa-linkedin-square"></span></a>
+
