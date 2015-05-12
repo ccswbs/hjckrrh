9ad19fade12d162e317f4a1aca18e9a3f26dd0e5
M. Brent Harp <brharp@uoguelph.ca>
2015-05-12 15:30:47 -0400

    Add feature content type and view.

---
+++ profiles/ug/themes/ug/ug_theme/templates/views-view-fields--ft1.tpl.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @file
+ * FT1 - Featured item
+ *
+ * Available variables:
+ * - $title: title of featured item
+ * - $image: feature image
+ * - $link: link to featured item
+ *
+ * Other variables:
+ * - $view: The view in use.
+ * - $fields: an array of $field objects. Each one contains:
+ *   - $field->content: The output of the field.
+ *   - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
+ *   - $field->class: The safe class id to use.
+ *   - $field->handler: The Views field handler object controlling this field. Do not use
+ *     var_export to dump this object, as it can't handle the recursion.
+ *   - $field->inline: Whether or not the field should be inline.
+ *   - $field->inline_html: either div or span based on the above flag.
+ *   - $field->wrapper_prefix: A complete wrapper containing the inline_html to use.
+ *   - $field->wrapper_suffix: The closing tag for the wrapper.
+ *   - $field->separator: an optional separator that may appear before a field.
+ *   - $field->label: The wrap label text to use.
+ *   - $field->label_html: The full HTML of the label to use including
+ *     configured element type.
+ * - $row: The raw result object from the query, with all data it fetched.
+ *
+ */
+?>
+<div class="thumbnail">
+  <a href="<?php print $link; ?>">
+    <?php print $image; ?>
+    <div class="caption">
+      <p><?php print $title; ?></p>
+    </div>
+  </a>
+</div>
+
