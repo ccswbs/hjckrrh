090d58f9250554326021a54bdc412917f3fe84d7
M. Brent Harp <brharp@uoguelph.ca>
2015-06-08 09:59:36 -0400

    Move 'pause/play' to one button on slideshow.

--- profiles/ug/themes/ug/ug_theme/templates/views-view-unformatted--b1--autoplay.tpl.php
+++ profiles/ug/themes/ug/ug_theme/templates/views-view-unformatted--b1--autoplay.tpl.php
@@ -5,46 +5,46 @@
  * B1 - Image slider (banner)
  */
 ?>
-<div class="panel panel-default"><div class="panel-body">
 <div id="slides">
   <?php foreach ($rows as $id => $row): ?>
     <?php print $row; ?>
   <?php endforeach; ?>
   <div class="row slidesjs-navigation">
     <div class="col-sm-6">
+      <div class="btn-group" role="group">
+        <button class="btn btn-default slidesjs-psply">
+          <span class="slidesjs-stop">
+            <span class="glyphicon glyphicon-pause" aria-hidden="true"></span>
+            <span class="sr-only"><?php print t('Pause slideshow'); ?></span>
+          </span>
+          <span class="slidesjs-play" style="display:none;">
+            <span class="glyphicon glyphicon-play" aria-hidden="true"></span>
+            <span class="sr-only"><?php print t('Play slideshow'); ?></span>
+          </span>
+        </button>
+      </div>
       <a href="#" class="btn btn-link slidesjs-slide-link slidesjs-slide-title"></a>
     </div>
     <div class="col-sm-6">
       <div class="btn-toolbar pull-right" role="toolbar">
         <div class="btn-group" role="group">
-          <a href="#slides" class="btn btn-default slidesjs-previous" role="button">
+          <button class="btn btn-default slidesjs-previous" role="button">
             <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-            <span class="sr-only"><?php print t('Previous'); ?></span>
-          </a>
+            <span class="sr-only"><?php print t('Previous slide'); ?></span>
+          </button>
           <div class="btn btn-default" disabled>
             <?php print t('Slide <span class="slidesjs-slide-number">@number</span> of @count',
                         array('@number' => $slide_number, '@count' => $slide_count)); ?>
           </div>
-          <a href="#slides" class="btn btn-default slidesjs-next" role="button">
+          <button class="btn btn-default slidesjs-next" role="button">
             <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-            <span class="sr-only"><?php print t('Next'); ?></span>
-          </a>
-        </div>
-        <div class="btn-group" role="group">
-          <a href="#slides" class="btn btn-default slidesjs-stop" role="button">
-            <span class="glyphicon glyphicon-pause" aria-hidden="true"></span>
-            <span class="sr-only"><?php print t('Pause'); ?></span>
-          </a>
-          <a href="#slides" class="btn btn-default slidesjs-play" role="button" disabled>
-            <span class="glyphicon glyphicon-play" aria-hidden="true"></span>
-            <span class="sr-only"><?php print t('Play'); ?></span>
-          </a>
+            <span class="sr-only"><?php print t('Next slide'); ?></span>
+          </button>
         </div>
       </div>
     </div>
   </div>
 </div>
-</div></div>
 <script src="//www.uoguelph.ca/js/jquery.slides.min.js"></script>
 <script>
   jQuery(function($) {
@@ -74,15 +74,21 @@
         complete: update,
       },
     });
-    $('.slidesjs-stop').click(function () {
-      $('#slides').first().data('plugin_slidesjs').stop();
-      $('.slidesjs-stop').attr('disabled', 'disabled');
-      $('.slidesjs-play').removeAttr('disabled');
+    $('.slidesjs-next, .slidesjs-previous').click(function () {
+      $('.slidesjs-stop').hide();
+      $('.slidesjs-play').show();
     });
-    $('.slidesjs-play').click(function () {
-      $('#slides').first().data('plugin_slidesjs').play(true);
-      $('.slidesjs-play').attr('disabled', 'disabled');
-      $('.slidesjs-stop').removeAttr('disabled');
+    $('.slidesjs-psply').click(function () {
+      var plugin = $('#slides').first().data('plugin_slidesjs');
+      if ($.data(plugin, 'playing')) {
+        plugin.stop();
+        $('.slidesjs-stop').hide();
+        $('.slidesjs-play').show();
+      } else {
+        plugin.play(true);
+        $('.slidesjs-play').hide();
+        $('.slidesjs-stop').show();
+      }
     });
   });
 </script>
