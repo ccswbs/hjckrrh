75d0c10fe575db8a5d77ef5450502a23518b8e1b
mmafe <miranda.mafe@gmail.com>
2015-08-06 16:54:19 -0400

    Update script to set aria-expanded to true on open

--- profiles/ug/themes/ug/ug_theme/js/ug_scripts.js
+++ profiles/ug/themes/ug/ug_theme/js/ug_scripts.js
@@ -7,6 +7,20 @@ Dropdown Menus
 	---------------------**/
 	jQuery(
 	  	function($) {
+
+		// On dropdown open
+		$(document).on('shown.bs.dropdown', function(event) {
+		  var dropdown = $(event.target);
+		 
+		  // Set aria-expanded to true
+		  dropdown.find('.dropdown-menu').attr('aria-expanded', true);
+		 
+		  // Set focus on the first link in the dropdown
+		  setTimeout(function() {
+		    dropdown.find('.dropdown-menu li:first-child a').focus();
+		  }, 10);
+		});
+	  		
 		// On dropdown close
 			$(document).on('hidden.bs.dropdown', function(event) {
 			 var dropdown = $(event.target);
