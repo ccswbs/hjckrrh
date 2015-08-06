c62d18d4610af899b6f3aeff82654731b18eecc1
mmafe <miranda.mafe@gmail.com>
2015-08-06 16:37:26 -0400

    Add script to announce dropdown state for screen-readers

---
+++ profiles/ug/themes/ug/ug_theme/js/ug_scripts.js
@@ -0,0 +1,18 @@
+/*************************
+Dropdown Menus
+*************************/
+
+
+  /*---------------------
+  Expanded State Fix
+  ---------------------**/
+	// On dropdown close
+	$(document).on('hidden.bs.dropdown', function(event) {
+	 var dropdown = $(event.target);
+	 
+	 // Set aria-expanded to false 
+	 dropdown.find('.dropdown-menu').attr('aria-expanded', false);
+	 
+	 // Set focus back to dropdown toggle
+	 dropdown.find('.dropdown-toggle').focus();
+	});
