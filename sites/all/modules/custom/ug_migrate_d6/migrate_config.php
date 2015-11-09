87ac42d8c1a6b9334399ec71dffb021cdb52d001
mmafe <miranda.mafe@gmail.com>
2015-11-09 14:02:00 -0500

    Use backup variables if site migrate_settings file does not exist

--- sites/all/modules/custom/ug_migrate_d6/migrate_config.php
+++ sites/all/modules/custom/ug_migrate_d6/migrate_config.php
@@ -7,14 +7,16 @@
 
   /* USER SETTINGS */
   $role_mappings = array(
-    'source-d6-editor' => 'editor',
+    //'source-d6-editor' => 'editor',
   );
 
   /* TAXONOMY Settings */
-  $term_keyword_source = '2'; //comuspicloto'
-  $term_page_category_source = '3';
-  $term_news_keyword_source = '2'; //comuspicloto'
-  $term_news_category_source = '1'; //locl
+  //Note: for D6, use term id
+
+  $term_keyword_source = ''; 
+  $term_page_category_source = ''; 
+  $term_news_keyword_source = '';
+  $term_news_category_source = '';
 
   /* PAGE Settings */
   $node_page_type_source = 'page';
@@ -22,8 +24,8 @@
   $page_arguments = array(
   	'source_page_body' => 'body',
     'source_page_format' => 'body:format',
-    'source_page_category' => '3',
-    'source_page_keyword' => '2',
+    'source_page_category' => '',
+    'source_page_keyword' => '',
   );
 
   /* NEWS Settings */
@@ -32,12 +34,12 @@
   $news_arguments = array(
   	'source_news_body' => 'body',
     'source_news_format' => 'body:format',
-	'source_news_category' => '1', //locl
-	'source_news_keyword' => '2', //comuspicloto'
+	'source_news_category' => '',
+	'source_news_keyword' => '',
   );
 
   /* Picture Settings */
-  $picture_source = '/Applications/MAMP/htdocs/tempsite/drupal-6.37/';
+  $picture_source = '';
   $picture_destination = 'public://';
 
   /************************************************/
