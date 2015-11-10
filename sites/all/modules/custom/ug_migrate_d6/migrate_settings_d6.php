72fc39a903c55727e6edaa8a2650c79058a05153
mmafe <miranda.mafe@gmail.com>
2015-11-10 13:44:21 -0500

    Register event node/category/keyword in UG Migrate D6

--- sites/all/modules/custom/ug_migrate_d6/migrate_settings_d6.php
+++ sites/all/modules/custom/ug_migrate_d6/migrate_settings_d6.php
@@ -10,13 +10,20 @@
     //'source-d6-editor' => 'editor',
   );
 
+  /* Picture Settings */
+  $picture_source = '';
+  $picture_destination = 'public://';
+
   /* TAXONOMY Settings */
   //Note: for D6, use term id
 
   $term_keyword_source = ''; 
-  $term_page_category_source = ''; 
   $term_news_keyword_source = '';
+  $term_event_keyword_source = '';
+
+  $term_page_category_source = ''; 
   $term_news_category_source = '';
+  $term_event_category_source = '';
 
   /* PAGE Settings */
   $node_page_type_source = 'page';
@@ -34,13 +41,19 @@
   $news_arguments = array(
   	'source_news_body' => 'body',
     'source_news_format' => 'body:format',
-	'source_news_category' => '',
-	'source_news_keyword' => '',
+  	'source_news_category' => '',
+  	'source_news_keyword' => '',
   );
 
-  /* Picture Settings */
-  $picture_source = '';
-  $picture_destination = 'public://';
+  /* EVENT Settings */
+  $node_event_type_source = '';
+
+  $event_arguments = array(
+    'source_event_body' => 'body',
+    'source_event_format' => 'body:format',
+    'source_event_category' => '',
+    'source_event_keyword' => '',
+  );
 
   /************************************************/
 
