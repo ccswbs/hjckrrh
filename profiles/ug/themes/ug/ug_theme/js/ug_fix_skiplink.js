43cbfb3a3dd6fc4fb249a2bdc2a5f8d0d2435e6e
abanuog <abanerji@uoguelph.ca>
2016-02-11 13:58:44 -0500

    Fix: Anchors (Including skip to main content) don't work in Chrome [refs #27]

---
+++ profiles/ug/themes/ug/ug_theme/js/ug_fix_skiplink.js
@@ -0,0 +1,19 @@
+
+jQuery(
+   function($) {
+ 
+    // add a click handler to all links 
+    // that point to same-page targets (href="#...")		
+    $("a[href^='#']").click(function() {
+      // get the href attribute of the internal link
+      // then strip the first character off it (#)
+      // leaving the corresponding id attribute
+      $("#"+$(this).attr("href").slice(1)+"")
+        // give that id focus (for browsers that didn't already do so)
+        .focus()
+	.attr("tabIndex",-1)
+        // add a highlight effect to that id (comment out if not using)			
+        .effect("highlight", {}, 3000);
+    });
+  });
+
