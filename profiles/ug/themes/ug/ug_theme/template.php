471b37f773bd78054c58fad98fedeb7fe443dbe6
M. Brent Harp <brharp@uoguelph.ca>
2016-05-25 09:46:47 -0400

    Fix: Remove empty name attribute from button (ug_profile)[#386]

--- profiles/ug/themes/ug/ug_theme/template.php
+++ profiles/ug/themes/ug/ug_theme/template.php
@@ -946,3 +946,49 @@ function ug_theme_preprocess_views_view__n3(&$vars) {
 }
 
 
+function ug_theme_form_required_marker($variables) {
+  // This is also used in the installer, pre-database setup.
+  $t = get_t();
+  $attributes = array(
+    'class' => 'form-required',
+    'title' => $t('This field is required.'),
+  );
+  return '<span' . drupal_attributes($attributes) . '>(' . $t('required') . ')</span>';
+}
+
+
+/**
+ * Overrides theme_button().
+ */
+function ug_theme_button($variables) {
+  $element = $variables['element'];
+  $label = $element['#value'];
+  element_set_attributes($element, array('id', 'name', 'value', 'type'));
+
+  // If a button type class isn't present then add in default.
+  $button_classes = array(
+    'btn-default',
+    'btn-primary',
+    'btn-success',
+    'btn-info',
+    'btn-warning',
+    'btn-danger',
+    'btn-link',
+  );
+  $class_intersection = array_intersect($button_classes, $element['#attributes']['class']);
+  if (empty($class_intersection)) {
+    $element['#attributes']['class'][] = 'btn-default';
+  }
+
+  // Add in the button type class.
+  $element['#attributes']['class'][] = 'form-' . $element['#button_type'];
+
+  // Remove name attribute if empty, for W3C compliance.
+  if (isset($element['#attributes']['name']) && $element['#attributes']['name'] === '') {
+    unset($element['#attributes']['name']);
+  }
+
+  // This line break adds inherent margin between multiple buttons.
+  return '<button' . drupal_attributes($element['#attributes']) . '>' . $label . "</button>\n";
+}
+
