f5f18e7d47d870769d51895f5132a5e7fed72ed2
Tanya Qureshi <tqureshi@ws-securehst13.cs.uoguelph.ca>
2016-05-26 13:26:38 -0500

    New: Course outline feature

--- profiles/ug/themes/ug/ug_theme/template.php
+++ profiles/ug/themes/ug/ug_theme/template.php
@@ -993,3 +993,54 @@ function ug_theme_button($variables) {
   return '<button' . drupal_attributes($element['#attributes']) . '>' . $label . "</button>\n";
 }
 
+
+/**
+ * Overrides theme_form_element_label().
+ */
+function ug_theme_form_element_label(&$variables) {
+  $element = $variables['element'];
+
+  // This is also used in the installer, pre-database setup.
+  $t = get_t();
+
+  // Determine if certain things should skip for checkbox or radio elements.
+  $skip = (isset($element['#type']) && ('checkbox' === $element['#type'] || 'radio' === $element['#type']));
+
+  // If title and required marker are both empty, output no label.
+  if ((!isset($element['#title']) || $element['#title'] === '' && !$skip) && empty($element['#required'])) {
+    return '';
+  }
+
+  // If the element is required, a required marker is appended to the label.
+  $required = !empty($element['#required']) ? theme('form_required_marker', array('element' => $element)) : '';
+
+  $title = filter_xss_admin($element['#title']);
+
+  $attributes = array();
+
+  // Style the label as class option to display inline with the element.
+  if ($element['#title_display'] == 'after' && !$skip) {
+    $attributes['class'][] = $element['#type'];
+  }
+  // Show label only to screen readers to avoid disruption in visual flows.
+  elseif ($element['#title_display'] == 'invisible' && empty($element['#id'])) {
+    $attributes['class'][] = 'element-invisible';
+  }
+
+  if (!empty($element['#id'])) {
+    $attributes['for'] = $element['#id'];
+  }
+
+  // Insert radio and checkboxes inside label elements.
+  $output = '';
+  if (isset($variables['#children'])) {
+    $output .= $variables['#children'];
+  }
+
+  // Append label.
+  $output .= $t('!title !required', array('!title' => $title, '!required' => $required));
+
+  // The leading whitespace helps visually separate fields from inline labels.
+  return ' <label' . drupal_attributes($attributes) . '>' . $output . "</label>\n";
+}
+
