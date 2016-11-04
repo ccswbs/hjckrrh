e1ea04e021abaacc7789997da5c6f8e6ccd2a8cd
abanuog <abanerji@uoguelph.ca>
2016-11-04 14:02:48 -0400

    removed meta charset tag from html.tpl.php

--- profiles/ug/themes/ug/ug_cornerstone/templates/html.tpl.php
+++ profiles/ug/themes/ug/ug_cornerstone/templates/html.tpl.php
@@ -47,7 +47,6 @@
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces;?>>
 <!-- <head profile="<?php /*print $grddl_profile;*/ ?>"> -->
 <head>
-  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
