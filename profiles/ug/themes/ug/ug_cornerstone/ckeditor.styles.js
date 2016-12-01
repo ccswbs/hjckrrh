bc95202f6e73b507eaa26283c3653136c7c42bbb
Tanya Qureshi <tqureshi@uoguelph.ca>
2016-12-01 08:47:13 -0600

    Styles menu added but doesn't work with existing code

--- profiles/ug/themes/ug/ug_cornerstone/ckeditor.styles.js
+++ profiles/ug/themes/ug/ug_cornerstone/ckeditor.styles.js
@@ -30,3 +30,37 @@ CKEDITOR.on( 'dialogDefinition', function( ev ) {
         classField[ 'default' ] = 'img-responsive';
     }
 });
+
+/*
+Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+/*
+ * This file is used/requested by the 'Styles' button.
+ * The 'Styles' button is not enabled by default in DrupalFull and DrupalFiltered toolbars.
+ */
+if(typeof(CKEDITOR) !== 'undefined') {
+    CKEDITOR.addStylesSet( 'drupal',
+    [
+            /* Object Styles */
+
+            {
+                    name : 'Image on Left',
+                    element : 'img',
+                    attributes :
+                    {
+                            'class' : 'pull-left'
+                    }
+            },
+
+            {
+                    name : 'Image on Right',
+                    element : 'img',
+                    attributes :
+                    {
+                            'class' : 'pull-right'
+                    }
+            }
+    ]);
+}
