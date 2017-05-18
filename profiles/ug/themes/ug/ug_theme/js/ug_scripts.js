05cebbc8d2921e84b3654dd407450da2b019afea
Nevan Tan <nevan@tanclan.ca>
2017-05-18 12:44:40 -0400

    Another update to automate more attributes

--- profiles/ug/themes/ug/ug_theme/js/ug_scripts.js
+++ profiles/ug/themes/ug/ug_theme/js/ug_scripts.js
@@ -15,7 +15,8 @@ jQuery(function($) {
 			var controller = $('[data-target="#' + $(elem).attr("id") + '"]');
 
 			// Set aria-controls on controlling element
-			if(!controller.attr("aria-controls")) controller.attr("aria-controls", $(elem).attr("id"));
+			controller.attr("aria-controls", $(elem).attr("id"));
+			controller.attr("data-toggle", "collapse");
 
 			// If controller is a link, also set href to target id
 			if(controller.get(0).tagName.toLowerCase() == "a") controller.attr("href", $(elem).attr("id"));
