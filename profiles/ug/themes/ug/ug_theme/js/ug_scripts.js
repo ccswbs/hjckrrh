696c50cb98b057fb93c772c00dcbd6f4b8c8309e
Nevan Tan <nevan@tanclan.ca>
2017-05-18 10:10:56 -0400

    Update javascript-enabled attributes

--- profiles/ug/themes/ug/ug_theme/js/ug_scripts.js
+++ profiles/ug/themes/ug/ug_theme/js/ug_scripts.js
@@ -3,10 +3,22 @@ Bootstrap collapsible aria-expanded fix
 ------------------------------------**/
 jQuery(function($) {
 	$(document).ready(function(e) {
+		// For each collapsible element
 		$(".collapse").each(function(i, elem) {
+			// Determine if element is collapsed by default
 			var isExpanded = $(elem).hasClass('in');
+
+			// Set aria-expanded on collapsible container
 			$(elem).attr("aria-expanded", isExpanded);
-			$('[data-target="#' + $(elem).attr("id") + '"]').attr("aria-expanded", isExpanded);
+
+			// Select controlling element
+			var controller = $('[data-target="#' + $(elem).attr("id") + '"]');
+
+			// Set aria-controls on controlling element
+			if(!controller.attr("aria-controls")) controller.attr("aria-controls", $(elem).attr("id"));
+
+			// If controller is a link, also set href to target id
+			if(controller.get(0).tagName.toLowerCase() == "a") controller.attr("href", $(elem).attr("id"));
 		});
 	});
 });
