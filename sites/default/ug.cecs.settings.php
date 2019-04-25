417cf896871897cc78c3e6e84da2a822d85e9874
M. Brent Harp <brharp@uoguelph.ca>
2019-04-25 13:40:52 -0400

    Fix base path for studentaffairs and ses

--- sites/default/ug.cecs.settings.php
+++ sites/default/ug.cecs.settings.php
@@ -1,6 +1,5 @@
 <?php
-
-if ($_ENV['PANTHEON_ENVIRONMENT'] === 'live') {
-  $ug_base_path = '/cecs';
-  $ug_primary_domain = 'www.recruitguelph.ca';
+if ($_SERVER["HTTP_HOST"] === "live-cecs.pantheonsite.io") {
+  $ug_base_path = "/cecs";
+  $ug_primary_domain = "www.recruitguelph.ca";
 }
--- sites/default/ug.ses1.settings.php
+++ sites/default/ug.ses1.settings.php
@@ -1,4 +1,4 @@
 <?php
 if ($_SERVER["HTTP_HOST"] === "live-ses1.pantheonsite.io") {
-  $ug_base_path = "/ses1";
+  $ug_base_path = "/ses";
 }
