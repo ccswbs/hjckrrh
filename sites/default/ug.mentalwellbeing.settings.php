e38ea6c3991a3a8674316bed81a103c97b983645
M. Brent Harp <brharp@uoguelph.ca>
2019-06-05 15:10:28 -0400

    Change platform URL for mentalwellbeing

--- sites/default/ug.mentalwellbeing.settings.php
+++ sites/default/ug.mentalwellbeing.settings.php
@@ -1,5 +1,5 @@
 <?php
-if ($_SERVER["HTTP_HOST"] === "live-mentalwellbeing.pantheonsite.io") {
+if ($_SERVER["HTTP_HOST"] === "mentalwellbeing.uoguelph.ca") {
   $ug_base_path = "/mentalwellbeing";
   $ug_primary_domain = "www.uoguelph.ca";
 }
