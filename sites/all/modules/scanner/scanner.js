2a0a73b0fcf5868b65b26d6dfd31c6da74eb0c04
Miranda Mafe <miranda.mafe@gmail.com>
2019-07-17 09:19:01 -0400

    Update Search and Replace Scanner to version 1.1

--- sites/all/modules/scanner/scanner.js
+++ sites/all/modules/scanner/scanner.js
@@ -1,17 +1,21 @@
+/**
+ * @file
+ * Custom JS for the Search and Replace Scanner module.
+ */
 (function ($) {
 
-/**
- * Prevents submit button double-click on replace confirmation form.
- */
-Drupal.behaviors.hideSubmitButton = {
-  attach: function(context) {
-    $('input#edit-confirm').click(function() {
-      $('.scanner-buttons').css('position','relative')
-        .append('<div class="scanner-button-msg"><p>Replacing items... please wait...</p></div>')
-      $('.scanner-button-msg').click(function() { return false; });
-      return true;
-    });
-  }
-};
+  /**
+   * Prevents submit button double-click on replace confirmation form.
+   */
+  Drupal.behaviors.hideSubmitButton = {
+    attach: function(context) {
+      $('input#edit-confirm').click(function() {
+        $('.scanner-buttons').css('position','relative')
+          .append('<div class="scanner-button-msg"><p>Replacing items... please wait...</p></div>')
+        $('.scanner-button-msg').click(function() { return false; });
+        return true;
+      });
+    }
+  };
 
 })(jQuery);
