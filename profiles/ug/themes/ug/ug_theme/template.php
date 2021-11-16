6b0b748d5d1da7cd83b2d0d3971cd2f4ab6ca5a3
abanuog <abanerji@uoguelph.ca>
2021-11-16 13:49:32 -0500

    removed role=menu from template.php

--- profiles/ug/themes/ug/ug_theme/template.php
+++ profiles/ug/themes/ug/ug_theme/template.php
@@ -727,7 +727,7 @@ function ug_theme_menu_link(array $variables) {
     elseif ((!empty($element['#original_link']['depth'])) && ($element['#original_link']['depth'] == 1)) {
       // Add our own wrapper.
       unset($element['#below']['#theme_wrappers']);
-      $sub_menu = '<ul class="dropdown-menu" role="menu">' . drupal_render($element['#below']) . '</ul>';
+      $sub_menu = '<ul class="dropdown-menu">' . drupal_render($element['#below']) . '</ul>';
       // Generate as standard dropdown.
       $element['#title'] .= ' <span class="caret"></span>';
       $element['#attributes']['class'][] = 'dropdown';
